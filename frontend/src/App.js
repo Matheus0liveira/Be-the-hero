f03153255e6087b3c3f1465beb0ab47bb3b26174
Matheus Oliveira <matheusfilho98.ms@gmail.com>
2020-03-25 18:35:55 -0300

    Back-end e Front-end da aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './global.css'
+import Routes from './routes'
+
+// JSX : (JavaScript XML) == é HTML escrito dentro do JS
+//  XML é a sintaxe do HTML
+
+
+
+function App() {
+
+  return (
+   <Routes/>
+
+  );
+}
+
+export default App;
